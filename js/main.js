bd3f303ba848e32f034490992ac2fa92f18bf354
vladdoletskiy <vladdoletskiy@gmail.com>
2021-02-04 18:57:08 +0200

    burgundia

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('.photo__slider').slick({
+        prevArrow: '<button type="button" class="slick-prev"><img src="/img/left.png" alt="1"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="/img/Right.png" alt="2"></button>',
+        
+    });
+});
+
